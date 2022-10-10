2f008fa9e5ffbec9df17b9cd4735acebe277f072
vipereir <zzvtrx@gmail.com>
2022-10-10 12:51:34 -0300

    entendendo o mutex

--- philo/philo.c
+++ philo/philo.c
@@ -1,34 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophia.c                                      :+:      :+:    :+:   */
+/*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 10:02:23 by vipereir          #+#    #+#             */
-/*   Updated: 2022/10/10 11:44:54 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/10/10 12:51:14 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophia.h"
 
-void *print_test(void *a)
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void *test_func(void *a)
 {
 	int	i;
+	long c;
 
-	i = 0;
-	while (i < 5)
+	pthread_mutex_lock(&lock);
+	c = (long)a;
+	i = -1;
+	while (i++ < 5)
 	{
-		printf("%i\n", i);
-		usleep(100);
-		i++;
+		c++;
+		printf("%li\n", c);
 	}
-	return (NULL);
+	pthread_mutex_unlock(&lock);
+	return ((void *)c);
 }
 
 void	t_error(char *msg)
 {
-	ft_printf("%mgs\n");
+	printf("%mgs\n");
 	exit(1);
 }
 
@@ -37,16 +42,20 @@ int	main(void)
 	pthread_t	philo[number_of_philosophers];
 	void	*result;
 	int i;
+	static long c;
 
 	i = -1;
 	while (i++ < number_of_philosophers)
 	{
-		if (pthread_create(&philo[i], NULL, print_test, NULL) == -1)
+		if (pthread_create(&philo[i], NULL, test_func, (void *)c) == -1)
 			t_error("pthread create error!");
 	}
 	i = -1;
-	while 
-
-
+	while (i++ < number_of_philosophers)
+	{
+		pthread_join(philo[i], &result);
+		c += (long)result;
+		printf("c: %li\n", c);
+	}
 	return (0);
 }
