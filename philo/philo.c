cbd5a158c13afdcee75a47cee5b63f81e6689126
vipereir <zzvtrx@gmail.com>
2022-11-10 14:36:08 -0300

    fazendo primeiro malloc

--- philo/philo.c
+++ philo/philo.c
@@ -6,16 +6,37 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 11:48:07 by vipereir          #+#    #+#             */
-/*   Updated: 2022/11/10 11:40:47 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/11/10 14:34:24 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	ft_malloc_zero(t_logic *logic)
+{
+	logic = malloc(sizeof(t_logic *));
+	if (!logic)
+		return (-1);
+	logic->philos = malloc(sizeof(pthread_t) * logic->number_phi);
+	if (!logic->philos)
+		return (-1);
+	logic->forks = malloc(sizeof(pthread_mutex_t) * logic->number_phi);
+	if (!logic->forks)
+		return (-1);
+	memset(logic, 0x0, sizeof(t_logic));
+	return (0);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_logic			logic;
 
-	memset(&logic, 0x0, sizeof(logic));
+	if (ft_malloc_zero(&logic) == -1)
+	{
+		printf("malloc_error\n");
+		return (0);
+	}
+
+
 return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:07:38 by vipereir          #+#    #+#             */
-/*   Updated: 2022/11/10 10:34:00 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/11/10 11:48:16 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,16 @@
 typedef struct	s_logic{
 	/* game rules */
 
-	int	number_phi;
-	int	t_die;
-	int	t_eat;
-	int	t_slp;
-	int	number_eat;
+	int				number_phi;
+	int				t_die;
+	int				t_eat;
+	int				t_slp;
+	int				number_eat;
+
+	/* philosophers and forks arrays*/
+
+	pthread_t		*philos;
+	pthread_mutex_t	*forks;
 
 }				t_logic;
 
