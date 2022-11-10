85f1a3c13435315a2a41606e0a41d97606d85ee8
vipereir <zzvtrx@gmail.com>
2022-11-10 11:40:22 -0300

    aprendendo a usar o memset para zerar

--- philo/philo.c
+++ philo/philo.c
@@ -1,17 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.c                                     :+:      :+:    :+:   */
+/*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 11:48:07 by vipereir          #+#    #+#             */
-/*   Updated: 2022/11/09 11:48:37 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/11/10 11:40:15 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+void print_args(t_logic *logic)
+{
+	printf("%d\n", logic->number_phi);
+	printf("%d\n", logic->t_die);
+	printf("%d\n", logic->t_eat);
+	printf("%d\n", logic->t_slp);
+	printf("%d\n", logic->number_eat);
+}
+
 int	main(int argc, char *argv[])
 {
+	t_logic			logic;
+	unsigned char	c;
 
-	return (0);
+	logic.t_eat = 0x1;
+	print_args(&logic);
+	memset(&logic, 0x0, sizeof(logic));
+	print_args(&logic);
+return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:07:38 by vipereir          #+#    #+#             */
-/*   Updated: 2022/11/09 16:14:27 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/11/10 10:34:00 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,19 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <pthread.h>
+# include <string.h>
 # include <sys/time.h>
+
+typedef struct	s_logic{
+	/* game rules */
+
+	int	number_phi;
+	int	t_die;
+	int	t_eat;
+	int	t_slp;
+	int	number_eat;
+
+}				t_logic;
 
 int	ft_atoi(const char *str);
 
