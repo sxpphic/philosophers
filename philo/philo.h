010489ef0ccce5eb19d8c3b5d34d586db11322d1
vipereir <zzvtrx@gmail.com>
2022-11-21 11:42:52 -0300

    modifing the structures

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:07:38 by vipereir          #+#    #+#             */
-/*   Updated: 2022/11/21 10:57:51 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/11/21 11:40:59 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,27 @@
 # include <string.h>
 # include <sys/time.h>
 
-
-
 typedef struct	s_logic{
 	/* game rules */
 
 	int				number_phi;
-	int				t_die;
-	int				t_eat;
-	int				t_slp;
+	long			t_die;
+	long			t_eat;
+	long			t_slp;
 	int				number_eat;
 
 	/* philosophers and forks arrays*/
 
 	pthread_t		*philos;
 	pthread_mutex_t	*forks;
-
-	/* phi array */
+	pthread_mutex_t	print;
+	pthread_mutex_t	see_death;
 
 }				t_logic;
 
 typedef struct	s_phi{
 	int		index;
+	int		death;
 	t_logic	*logic;
 }t_phi;
 
