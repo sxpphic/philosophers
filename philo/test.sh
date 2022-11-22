ccc9f141cda22672868449549742e69d865983bb
vipereir <zzvtrx@gmail.com>
2022-11-22 11:58:56 -0300

    testerzinho michuruca kkkkk

---
+++ philo/test.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+
+if [ $1 == 1 ]
+then
+	echo "test ./philo 1 800 200 200"
+	echo "The philosopher should not eat and should die"
+	./philo 1 800 200 200
+	echo "end test"
+elif [ $1 -eq 2 ]
+then
+	echo "test ./philo 5 800 200 200"
+	echo "No philosopher should die"
+	./philo 5 800 200 200
+	echo "end test"
+elif [ $1 -eq 3 ]
+then
+	echo "test ./philo 5 800 200 200 7"
+	echo "No philosopher should die & end after 7 eats from each one"
+	./philo 5 800 200 200
+	echo "end test"
+elif [ $1 -eq 4 ]
+then
+	echo "test ./philo 4 410 200 200"
+	echo "No philosopher should die"
+	./philo 4 410 200 200
+	echo "end test"
+elif [ $1 -eq 5 ]
+then
+	echo "test ./philo 4 310 200 200"
+	echo "One philosopher should die"
+	./philo 4 410 200 200
+	echo "end test"
+fi
