c917f35a2b4725da5f801ea4f516260a83bf5d9c
vipereir <zzvtrx@gmail.com>
2022-11-24 15:57:11 -0300

    construindo as structs

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:07:38 by vipereir          #+#    #+#             */
-/*   Updated: 2022/11/23 14:22:17 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/11/24 15:56:39 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,33 +20,31 @@
 # include <sys/time.h>
 
 typedef struct	s_logic{
-	/* game rules */
-
 	int				number_phi;
 	long			t_die;
 	long			t_eat;
 	long			t_slp;
 	int				number_eat;
-
-	/* philosophers and forks arrays*/
-
-	/**/
-	pthread_t		*philos;
-	pthread_mutex_t	*forks;
-	int				*is_fork_locked;
-	pthread_mutex_t	print;
-	pthread_mutex_t	see_death;
-
 }				t_logic;
 
+typedef struct	s_table{
+	pthread_t		*philos;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print;
+}				t_table;
+
 typedef struct	s_phi{
-	int		index;
-	int		death;
-	long	last_eat;
-	t_logic	*logic;
-}t_phi;
 
-
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*philo;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*print;
+	int						id;
+	int						n_eats;
+	long					last_eat;
+	typedef struct s_logic	*logic;
+	typedef struct s_phi	*next;
+}				t_phi_list;
 
 long	get_time(void);
 int		ft_atoi(const char *str);
